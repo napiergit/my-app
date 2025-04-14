e6dcb4fedf44018bb66fbdccc923137d30287041
Andrew <napierandr@gmail.com>
2025-04-14 18:02:59 -0400

    WIP

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import solidPlugin from 'vite-plugin-solid';
 
 // SolidJS specific config
 export default defineConfig({
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify',  // Polyfill crypto
+    },
+  },
   plugins: [solidPlugin()],
   base: '/my-app/',  // Change this to your repo name (e.g., '/my-app/')
   build: {
